9b94a7d4a57e2084e1553ba5bb53f64052357659
GLOBBIMUS <striker1342566@gmail.com>
2017-11-20 20:03:55 -0600

    Implement the multy threaded method
    Method creates 4 threads and use runnable inner class. Each thread is
    responsible for it's section

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,10 +1,101 @@
 package mpd;
 
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
+	public int finalResult = Integer.MAX_VALUE;
 
-    @Override
-    public int minimumPairwiseDistance(int[] values) {
-        throw new UnsupportedOperationException();
-    }
 
+	public synchronized void setResult(int diff){
+		if(diff < finalResult){
+			finalResult = diff;
+		}
+	}
+
+	@Override
+	public int minimumPairwiseDistance(int[] values){ 
+
+		Thread[] threads = {new Thread(new Calculate("Lower_Left", values)), new Thread(new Calculate("Bottom_Right", values)), new Thread(new Calculate("Top_Right", values)), new Thread(new Calculate("Center", values))};
+		for(int i = 0; i < threads.length; i++){
+			threads[i].start();
+		}
+		try{
+		for(int i = 0; i < threads.length; i++){
+			threads[i].join();
+		}
+		}catch (InterruptedException e){
+			e.printStackTrace();
+		}
+		return finalResult;
+	}
+
+	private class Calculate implements Runnable{
+		String section = "";
+		int[] values;
+
+		public Calculate(String section, int[] values){
+			this.section = section;
+			this.values = values;
+		}
+
+		public void run(){
+			int diff = calculate(section, values);
+			setResult(diff);
+		} 
+
+		private int calculate(String section, int[] values){
+			int N  = values.length;
+			int halfOfN = N/2;
+			int result = Integer.MAX_VALUE;
+
+			if(section == "Lower_Left"){
+				for(int i = 0; i < halfOfN; i++){
+					for(int j = 0; j < i; j++){
+						int diff = Math.abs(values[i] - values[j]);
+						if (diff < result) {
+							result = diff;
+						}
+					}
+				}
+				return result; 
+
+			}else if(section == "Bottom_Right"){
+				for(int i = halfOfN; i < N; i++){
+					for(int j = 0; j+halfOfN < i; j++){
+						int diff = Math.abs(values[i] - values[j]);
+						if (diff < result) {
+							result = diff;
+						}
+					}
+				}
+				return result;
+
+			}else if(section == "Top_Right"){
+				for(int i = halfOfN; i < N; i++ ){
+					for(int j = halfOfN; j < i; j++){
+						int diff = Math.abs(values[i] - values[j]);
+						if (diff < result) {
+							result = diff;
+						}
+					}
+				}
+				return result;
+
+			}else if(section == "Center"){
+				for(int i = halfOfN; i < N; i++){
+					for(int j = 0; j + halfOfN < N; j++){
+						if((j+halfOfN) >= i){						
+							int diff = Math.abs(values[i] - values[j]);
+							if (diff < result) {
+								result = diff;
+							}
+						}
+					}
+				}
+				return result;
+
+			}else{
+				System.out.println("Error");
+				return -1;
+			}
+		}
+	}
 }
